3a42b57765e757a35b9b9b4f5ce459df3ad0d7dd
John321Blacksmith <kuzmichevi321@gmail.com>
2024-09-08 16:53:06 +0300

    cmd options were added

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
+import sys
 import asyncio
 from aiohttp import web
+from geo_app.db.transactions import create_table
 from geo_app.urls import routes
 
-# app realization
-app = web.Application()
 
-# combining the all the routes
-app.add_routes(*routes)
+def make_app() -> None:
+    # app realization
+    app = web.Application()
 
-if __name__ == '__main__':
+    # combining the all the routes
+    app.add_routes(routes)
 
     # launching the app
     web.run_app(app)
+
+
+def make_migrations() -> None:
+    """
+    Perform database migrations.
+    """
+    if sys.argv[2] == 'create-table':
+        create_table()
+
+
+def perform_commands() -> None:
+    command = sys.argv[1]
+    
+    if command == 'run-app':
+        return make_app()
+    
+    if command == 'makemigrations':
+        return make_migrations()
+
+
+if __name__ == '__main__':
+    perform_commands()
