befef2aa7cf8a26b322dcc1475650a77861ca502
John321Blacksmith <kuzmichevi321@gmail.com>
2024-09-09 03:12:45 +0300

    facilitated dist calculations

--- geo_app/db/transactions.py
+++ geo_app/db/transactions.py
@@ -1,11 +1,12 @@
-from typing import Coroutine
 from pathlib import Path
-from sqlalchemy import CursorResult, Engine, Tuple
+from sqlalchemy import Engine
 from sqlalchemy.orm import Session
 from sqlalchemy import (select, insert, update, delete)
 from sqlalchemy import Engine, create_engine
 from .models import CityBase, City
 from dotenv import load_dotenv
+from ..utils import sort_dists, distance
+
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -21,7 +22,7 @@ def setup_engine() -> Engine:
     return create_engine("sqlite+pysqlite:///cities1.db", echo=True)
 
 
-def create_table() -> Coroutine:
+def create_table() -> None:
     """
     Create a table scheme for
     the first time.
@@ -30,7 +31,7 @@ def create_table() -> Coroutine:
     CityBase.metadata.create_all(engine)
     
 
-async def add_row(**params) -> Coroutine:
+async def add_row(**params):
     """
     Create a new city object
     in the db.
@@ -52,18 +53,22 @@ async def add_row(**params) -> Coroutine:
             return None
 
 
-async def get_row(**params) -> Coroutine[Tuple[City]]:
+async def get_row(**params):
     """
-    Retrieve a db row via index.
+    Retrieve a db row via index or name.
     """
     engine = setup_engine()
 
     with Session(bind=engine) as session:
-        result = session.execute(select(City).where(City.id == params['id'])).first()
+        if params['id']:
+            result = session.execute(select(City).where(City.id == params['id'])).first()
+        if params['name']:
+            result = session.execute(select(City).where(City.name == params['name'])).first()
         return result if result else None
 
 
-async def update_row(**params) -> Coroutine[CursorResult]:
+
+async def update_row(**params):
     """
     Set new data to an existing row.
     """
@@ -74,7 +79,7 @@ async def update_row(**params) -> Coroutine[CursorResult]:
         return result if result else None
 
 
-async def delete_row(**params) -> Coroutine[CursorResult]:
+async def delete_row(**params):
     """
     Permanently delete a row
     from the database.
@@ -86,12 +91,25 @@ async def delete_row(**params) -> Coroutine[CursorResult]:
         return result if result else None
     
 
-async def get_nearby_rows(**params) -> Coroutine:
+async def get_nearest_rows(**params):
     """
-    Retrive two nearest cities
-    from the database.
+    Retrive a current obj and a list
+    of ones to make calculations.
     """
     engine = setup_engine()
     with Session(bind=engine) as session:
-        ...
+        current_city = session.execute(select(City).where(City.name == params['name'])).first()
+        cities = session.execute(select(City)).all()
+
+        # make a list of distances
+        distances: list[tuple[City, float]] = [(city, distance(current_city, city)) for city in cities]
+
+        # sort a list of distances 
+        sorted_dists: list[tuple[City, float]] = sort_dists(distances)
+
+        # and get the two nearest values
+        two_nearest: list[City] = [tup[0] for tup in sorted_dists[:2]]
+
+        return two_nearest
+
     
