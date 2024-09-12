acfb8164e339e6b9ceb8b8ee2cab72eff6b65ace
John321Blacksmith <kuzmichevi321@gmail.com>
2024-09-13 00:25:39 +0300

    database manager init-ed

--- geo_app/db/transactions.py
+++ geo_app/db/transactions.py
@@ -1,117 +1,121 @@
 from pathlib import Path
-from sqlalchemy import Engine
+from sqlalchemy import create_engine, Engine, text
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError, NoResultFound
 from sqlalchemy import (select, insert, update, delete)
-from sqlalchemy import Engine, create_engine
 from .models import CityBase, City
 from dotenv import load_dotenv
 from ..utils import sort_dists, distance
-
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 load_dotenv()
 env_path = BASE_DIR/'.env'
 load_dotenv(dotenv_path=env_path)
-
-
-def setup_engine() -> Engine:
-    """
-    Create an engine object
-    """
-    return create_engine("sqlite+pysqlite:///cities1.db", echo=True)
-
-
-def create_table() -> None:
-    """
-    Create a table scheme for
-    the first time.
-    """
-    engine = setup_engine()
-    CityBase.metadata.create_all(engine)
     
 
-async def add_row(**params):
+class DBManager:
     """
-    Create a new city object
-    in the db.
+    This manager controls
+    connection to the db
+    and applied transactions.
     """
-    engine = setup_engine()
-    with Session(bind=engine) as session:
-        if 'No Data' not in params.values():
-            city = City(
-                        name=params['name'],
-                        latitude=params['latitude'],
-                        longitude=params['longitude']
-                    )
-            session.add(city)
-            session.flush()
-            session.commit()
-
-            return city.id
-        else:
-            return None
-
-
-async def get_row(**params):
-    """
-    Retrieve a db row via index or name.
-    """
-    engine = setup_engine()
-
-    with Session(bind=engine) as session:
-        if params['id']:
-            result = session.execute(select(City).where(City.id == params['id'])).first()
-        elif params['name']:
-            result = session.execute(select(City).where(City.name == params['name'])).first()
-        else:
-            pass
-        return result if result else None
-
-
-
-async def update_row(**params):
-    """
-    Set new data to an existing row.
-    """
-    engine = setup_engine()
-    with Session(bind=engine) as session:
-        result = session.execute(update(City).where(City.id == params['id']).values(**params['body']))
-        session.commit()
-        return result if result else None
-
-
-async def delete_row(**params):
-    """
-    Permanently delete a row
-    from the database.
-    """
-    engine = setup_engine()
-    with Session(bind=engine) as session:
-        result = session.execute(delete(City).where(City.id == params['id']))
-        session.commit()
-        return result if result else None
+    def __init__(self, url: str) -> None:
+        self.url = url
+        self.engine = create_engine(self.url) # the engine remains up the server's life
     
-
-async def get_nearest_rows(**params):
-    """
-    Retrive a current obj and a list
-    of ones to make calculations.
-    """
-    engine = setup_engine()
-    with Session(bind=engine) as session:
-        current_city = session.execute(select(City).where(City.name == params['name'])).first()
-        cities = session.execute(select(City)).all()
-
-        # make a list of distances
-        distances: list[tuple[City, float]] = [(city, distance(current_city, city)) for city in cities]
-
-        # sort a list of distances 
-        sorted_dists: list[tuple[City, float]] = sort_dists(distances)
-
-        # and get the two nearest values
-        two_nearest: list[City] = [tup[0] for tup in sorted_dists[:2]]
-
-        return two_nearest
-
+    async def get_row(self, **params) -> dict[str, int|float] | None:
+        """
+        Retrieve a db row via index or name.
+        """
+        result: dict[str, int|float] | None = None
+        if self.engine:
+            with self.engine.connect() as connection:
+                try:
+                    queryset = connection.execute(select(City).where(City.id == params['id']))
+                    result = queryset.one()._mapping
+                except NoResultFound:
+                    return None
+        return result
     
+    async def insert_row(self, **params) -> int | None:
+        """
+        Create a new city object
+        in the db.
+        """
+        result: int | None = None
+        if self.engine:
+            with self.engine.connect() as connection:
+                if 'No Data' not in params.values():
+                    try:
+                        result = connection.execute(insert(City).values(**params))
+                    except IntegrityError as exc:
+                        print(exc.args)
+                        connection.rollback()
+                
+                    connection.commit()
+                    return result.rowcount
+        return result
+    
+    async def update_row(self, **params) -> int | None:
+        """
+        Set new data to an existing row.
+        """
+        result: int | None = None
+        if self.engine:
+            with self.engine.connect() as connection:
+                try:
+                    result = connection.execute(update(City)
+                                                .where(City.id == params['id'])
+                                                .values(**params['body']))
+                except IntegrityError as exc:
+                    print(exc.args)
+                    connection.rollback()
+                
+                connection.commit()
+                return result.rowcount
+        return result
+                
+    async def delete_row(self, **params) -> int | None:
+        """
+        Permanently delete a row
+        from the database.
+        """
+        result: int | None = None
+        if self.engine:
+            with self.engine.connect() as connection:
+                try:
+                    result = connection.execute(delete(City).where(City.id == params['id']))
+                except IntegrityError as exc:
+                    print(exc.args)
+                    connection.rollback()
+                connection.commit()
+                return result.rowcount
+        return result
+    
+    async def get_nearest_rows(self, **params) -> list[City] | None:
+        """
+        Retrive a current obj and a list
+        of ones to make calculations.
+        """
+        result: list[City] | None = None
+        if self.engine:
+            with self.engine.connect() as connection:
+                cr_city = connection.execute(select(City).where(City.name == params['name'])).first() # prevent multpl dupls
+                if cr_city:
+                    cities = connection.execute(select(City)).all()
+
+                    # make a list of distances, excluding current city
+                    distances: list[tuple[City, float]] = [
+                        (city, distance(cr_city, city)) for city in cities\
+                            if city._mapping['name'] != cr_city._mapping['name']
+                        ]
+
+                    # sort a list of distances
+                    sorted_dists: list[tuple[City, float]] = sort_dists(distances)
+
+                    # and get the two nearest values
+                    two_nearest: list[City] = [tup[0] for tup in sorted_dists[:2]]
+
+                    result = two_nearest
+        return result
